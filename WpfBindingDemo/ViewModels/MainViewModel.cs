598404243b701d8845843b8b35613de3f166c8b0
ve1ce <egor2006denisov@gmail.com>
2026-02-02 14:57:53 +0300

    все вкладки

--- WpfBindingDemo/ViewModels/MainViewModel.cs
+++ WpfBindingDemo/ViewModels/MainViewModel.cs
@@ -1,20 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.ObjectModel;
+using WpfBindingDemo.Views.Tabs;
 
 namespace WpfBindingDemo.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
-        // Это свойство будет содержать все вкладки
         public ObservableCollection<TabViewModel> Tabs { get; } = new ObservableCollection<TabViewModel>();
 
         public MainViewModel()
         {
-            // Позже добавим сюда создание вкладок
+            // Добавляем вкладки
+            AddTabs();
+        }
+
+        private void AddTabs()
+        {
+            // Вкладка OneTime Binding
+            var oneTimeViewModel = new OneTimeBindingViewModel();
+            var oneTimeView = new OneTimeBindingView();
+            oneTimeView.DataContext = oneTimeViewModel;
+
+            Tabs.Add(new TabViewModel("OneTime Binding", oneTimeView));
+
+            // Вкладка TwoWay Binding
+            var twoWayViewModel = new TwoWayBindingViewModel();
+            var twoWayView = new TwoWayBindingView();
+            twoWayView.DataContext = twoWayViewModel;
+
+            Tabs.Add(new TabViewModel("TwoWay Binding", twoWayView));
+
+            // Вкладка OneWay Binding
+            var oneWayViewModel = new OneWayBindingViewModel();
+            var oneWayView = new OneWayBindingView();
+            oneWayView.DataContext = oneWayViewModel;
+
+            Tabs.Add(new TabViewModel("OneWay Binding", oneWayView));
+
+            // Вкладка Default Binding
+            var defaultViewModel = new DefaultBindingViewModel();
+            var defaultView = new DefaultBindingView();
+            defaultView.DataContext = defaultViewModel;
+
+            Tabs.Add(new TabViewModel("Default Binding", defaultView));
+
+            // Вкладка Triggers
+            var triggersViewModel = new TriggersViewModel();
+            var triggersView = new TriggersView();
+            triggersView.DataContext = triggersViewModel;
+
+            Tabs.Add(new TabViewModel("Triggers", triggersView));
         }
     }
 }
---
+++ WpfBindingDemo/ViewModels/RelayCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Windows.Input;
+
+namespace WpfBindingDemo.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute?.Invoke() ?? true;
+
+        public void Execute(object parameter) => _execute();
+    }
+}
