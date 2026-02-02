901932cacd4b0d7cb7d9dfb232eb93d5668ec273
ve1ce <egor2006denisov@gmail.com>
2026-02-02 14:12:24 +0300

    Система папок

---
+++ WpfBindingDemo/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace WpfBindingDemo.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        // Это свойство будет содержать все вкладки
+        public ObservableCollection<TabViewModel> Tabs { get; } = new ObservableCollection<TabViewModel>();
+
+        public MainViewModel()
+        {
+            // Позже добавим сюда создание вкладок
+        }
+    }
+}
---
+++ WpfBindingDemo/ViewModels/TabViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfBindingDemo.ViewModels
+{
+    public class TabViewModel : ViewModelBase
+    {
+        private string _tabName;
+        private object _tabContent;
+
+        public string TabName
+        {
+            get => _tabName;
+            set => SetField(ref _tabName, value);
+        }
+
+        public object TabContent
+        {
+            get => _tabContent;
+            set => SetField(ref _tabContent, value);
+        }
+
+        public TabViewModel(string name, object content)
+        {
+            TabName = name;
+            TabContent = content;
+        }
+    }
+}
